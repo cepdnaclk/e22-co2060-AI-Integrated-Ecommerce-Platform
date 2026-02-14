fa3d5fce0b4c9fbe670e2b91f303851e969e67cd
Tharusha Inuwara <e22154@eng.pdn.ac.lk>
2026-02-14 15:56:49 +0530

    Commit 1

--- AI-ML/youtube-trending-system/run_daily_pipeline.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import pipeline.youtube_to_sheet
-import pipeline.sheet_to_storage
-import pipeline.update_history
-import pipeline.feature_engineering
-import pipeline.predict_trending
