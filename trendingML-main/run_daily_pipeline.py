a25bef43b82f61c71764658e125257bc532924ad
Geethma2003 <chandoopa2003@gmail.com>
2026-05-01 02:13:48 +0530

    Accuracy increased and workflow automated

---
+++ trendingML-main/run_daily_pipeline.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import pipeline.youtube_to_sheet
+import pipeline.sheet_to_storage
+import pipeline.update_history
+import pipeline.feature_engineering
+import pipeline.predict_trending
+#run_daily_pipeline.py
+#03:00 UTC - running the whole pipeline in sequence
+
---
+++ trendingML-main/run_pipeline.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\chand\Desktop\youtube-trending-system
+call venv\Scripts\activate
+python run_daily_pipeline.py
